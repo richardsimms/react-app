c2574aba617aa739d28e3cb5129979ffbacc8e50
Richard Simms <rich@R-I-C-H--K-A-T-E.local>
2018-08-10 12:06:06 +1000

    Removed paragraph

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Learn to create value in design</h1>
+    <p>Welcome to your new Gatsby site.</p>
+  
+    <Link to="/page-2/">Watch the video</Link>
+  </div>
+)
+
+export default IndexPage
