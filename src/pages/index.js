98b7f6658ec9bfd06afe8ca67acad5f2dbd7dd72
Richard Simms <richardsimms@RSimms.local>
2018-08-13 16:15:36 +1000

    Added the hero style basic styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,15 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Learn to create value in design</h1>
-    <p>Welcome to your new Gatsby site.</p>
-  
-    <Link to="/page-2/">Watch the video</Link>
+    <div>
+      <div className="Hero">
+        <div className="HeroGroup">
+          <h1>Learn to create value in design</h1>
+          <p>Welcome to your new Gatsby site.</p>
+          <Link to="/video">Watch the video</Link>
+        </div>
+      </div>
+    </div>
   </div>
 )
 
